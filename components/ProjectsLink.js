9846cd4ec6e8d1888172fe2ad3b50675666c602b
Chibuike Nnaji <67190735+chinnaji@users.noreply.github.com>
2022-06-07 00:39:20 +0100

    Update ProjectsLink.js

--- components/ProjectsLink.js
+++ components/ProjectsLink.js
@@ -3,10 +3,10 @@ import { BsGithub } from "react-icons/bs";
 import { BiLinkExternal } from "react-icons/bi";
 function ProjectsLink({ githubUrl, projectUrl }) {
   return (
-    <p className="mt-10 flex items-center gap-x-5  justify-center w-full text-4xl">
+    <p className="mt-10 flex flex-wrap  items-center px-3 md:gap-x-5  justify-center w-full text-4xl">
       <a
         href={githubUrl}
-        className="w-fit mx-auto lg:mx-0  items-center justify-center flex px-6 lg:px-12 text-base py-3  my-6 lg:my-9 bg-lime-500 hover:bg-lime-600 cursor-pointer rounded transition duration-100 ease-out hover:ease-in"
+        className="md:w-fit w-full  mx-auto lg:mx-0  items-center justify-center flex px-6 lg:px-12 text-base py-3  my-6 lg:my-9 bg-lime-500 hover:bg-lime-600 cursor-pointer rounded transition duration-100 ease-out hover:ease-in"
       >
         <span>
           <BsGithub className="text-xl" />
@@ -16,12 +16,12 @@ function ProjectsLink({ githubUrl, projectUrl }) {
 
       <a
         href={projectUrl}
-        className="w-fit mx-auto lg:mx-0  items-center justify-center flex px-6 lg:px-12 text-base py-3  my-6 lg:my-9 bg-lime-500 hover:bg-lime-600 cursor-pointer rounded transition duration-100 ease-out hover:ease-in"
+        className="md:w-fit w-full mx-auto lg:mx-0  items-center justify-center flex px-6 lg:px-12 text-base py-3  my-6 lg:my-9 bg-lime-500 hover:bg-lime-600 cursor-pointer rounded transition duration-100 ease-out hover:ease-in"
       >
         <span>
           <BiLinkExternal className="text-xl" />
         </span>
-        <span className="ml-2 font-semibold">Live🕵️‍♂️</span>
+        <span className="ml-2 font-semibold">Live Preview</span>
       </a>
     </p>
   );
