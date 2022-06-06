7ce09990214eee2208710b4e9a8d98e9cd1e67a9
Chibuike Nnaji <67190735+chinnaji@users.noreply.github.com>
2022-06-07 00:35:16 +0100

    Update individual buying styling

--- components/ProjectsLink.js
+++ components/ProjectsLink.js
@@ -6,7 +6,7 @@ function ProjectsLink({ githubUrl, projectUrl }) {
     <p className="mt-10 flex items-center gap-x-5  justify-center w-full text-4xl">
       <a
         href={githubUrl}
-        className="w-fit mx-auto lg:mx-0  items-center justify-center flex px-12 text-base py-3  my-6 lg:my-9 bg-lime-500 hover:bg-lime-600 cursor-pointer rounded transition duration-100 ease-out hover:ease-in"
+        className="w-fit mx-auto lg:mx-0  items-center justify-center flex px-6 lg:px-12 text-base py-3  my-6 lg:my-9 bg-lime-500 hover:bg-lime-600 cursor-pointer rounded transition duration-100 ease-out hover:ease-in"
       >
         <span>
           <BsGithub className="text-xl" />
@@ -16,7 +16,7 @@ function ProjectsLink({ githubUrl, projectUrl }) {
 
       <a
         href={projectUrl}
-        className="w-fit mx-auto lg:mx-0  items-center justify-center flex px-12 text-base py-3  my-6 lg:my-9 bg-lime-500 hover:bg-lime-600 cursor-pointer rounded transition duration-100 ease-out hover:ease-in"
+        className="w-fit mx-auto lg:mx-0  items-center justify-center flex px-6 lg:px-12 text-base py-3  my-6 lg:my-9 bg-lime-500 hover:bg-lime-600 cursor-pointer rounded transition duration-100 ease-out hover:ease-in"
       >
         <span>
           <BiLinkExternal className="text-xl" />
